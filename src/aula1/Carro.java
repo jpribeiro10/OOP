2142015f08a0c96bcfde1fc07902b65092f5dbe1
Joao Paulo Ribeiro <jpribeiro1010@gmail.com>
2025-08-15 15:13:28 -0300

    Primeiro commit aula 1

---
+++ src/aula1/Carro.java
@@ -0,0 +1,17 @@
+package aula1;
+
+public class Carro {
+
+    //atributos
+
+     String nome;
+     String marca;
+     int ano;
+     int vel;
+
+     //métodos
+
+    void acelerar(int aceleracao){
+        vel+=aceleracao;
+    }
+}
