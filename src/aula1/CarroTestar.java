2173f72ca08daffc7e6b979269fb046533db220b
Joao Paulo Ribeiro <jpribeiro1010@gmail.com>
2025-08-15 15:59:24 -0300

    Quarto commit aula 1 ;)

--- src/aula1/CarroTestar.java
+++ src/aula1/CarroTestar.java
@@ -1,5 +1,7 @@
 package aula1;
 
+import javax.tools.StandardLocation;
+
 public class CarroTestar {
     public static void main(String [] args){
 
@@ -9,6 +11,15 @@ public class CarroTestar {
         c1.marca = "Hyundai";
         c1.anoFab = 2021;
         c1.ano = 2021;
+        c1.vel = 50;
+
+        c1.acelerar(10);
+
+       // System.out.println("Velocidade: " + c1.vel + " Km/h");
+
+        c1.freiar(30);
+
+        System.out.println("Velocidade: " + c1.vel + " Km/h");
 
     }
 }
