b28d52c286f34000a06ddd23e83b9c91b3d7a12a
Joao Paulo Ribeiro <jpribeiro1010@gmail.com>
2025-08-18 23:00:24 -0300

    Primeiro commit aula 2

---
+++ src/aula2/Produto.java
@@ -0,0 +1,19 @@
+package aula2;
+
+import java.lang.String;
+
+public class Produto {
+    //atributos
+    String nome;
+    String marca;
+    float valor;
+
+    //construtores
+
+    Produto(String nome){
+        this.nome = ;
+
+    }
+
+
+}
